1451906faa7d91d5975977f57f07277f3dad00bb
mingggkeee <mksu234@naver.com>
2021-01-05 17:03:08 +0900

    '2021-01-05'

--- project1/subwayusage/admin.py
+++ project1/subwayusage/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from subwayusage.models import SubwayUsage
 
 # Register your models here.
+
+admin.site.register(SubwayUsage)
--- project1/subwayusage/models.py
+++ project1/subwayusage/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class SubwayUsage(models.Model):
+    Date = models.CharField(max_length=10)
+    SubLine = models.CharField(max_length=15)
+    SubStation = models.CharField(max_length=50)
+    pass_on_board = models.IntegerField
+    dise_pass = models.IntegerField
+
+    def __str__(self):
+        return self.SubStation
