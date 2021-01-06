bad8e11c158ea7aad5ddbea93d399b43c3c9cd32
mingggkeee <mksu234@naver.com>
2021-01-06 15:20:30 +0900

    '2021-01-06'

--- project1/subwayusage/views.py
+++ project1/subwayusage/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.generic import TemplateView, View
+
+from django.core import serializers
+
+from .models import SubwayUsage
+
 
 # Create your views here.
+
+class HomeView(TemplateView):
+    template_name = 'subwayusage/home.html'
